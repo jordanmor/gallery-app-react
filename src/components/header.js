09a7f0c8b8d179a1053583c73f542f64bb5de650
Jordan Mor <jordanmor@gmail.com>
2018-09-12 19:56:47 -0400

    Add SearchForm component

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from './nav';
+import SearchForm from './searchForm';
 
 const Header = props => {
   return ( 
     <header>
       <h1>Image Gallery</h1>
+      <SearchForm />
       <Nav />
     </header>
    );
