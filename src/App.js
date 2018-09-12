450f4a9658fa2a9af313cf7d09fa186a9ac13990
Jordan Mor <jordanmor@gmail.com>
2018-09-12 19:32:51 -0400

    Fetch data from Flickr API and update local state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import Gallery from './components/gallery';
+import apiKey from './.config';
 
 class App extends Component {
+  state = {
+      images: []
+  }
+
+  componentDidMount() {
+    this.search();
+  }
+
+  search = (tag = 'leaves') => {
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tag}&per_page=24&format=json&nojsoncallback=1`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          images: data.photos.photo
+        });
+      });
+  }
+
   render() {
+    const { images } = this.state;
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className="container">
+        <h1>Image Gallery</h1>
+        <Gallery images={images} />
       </div>
     );
   }
