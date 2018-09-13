d0310d45238213a466b0586829ef9356746963d9
Jordan Mor <jordanmor@gmail.com>
2018-09-12 20:08:18 -0400

    Display friendly user message when no search matches are found

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -3,6 +3,15 @@ import GalleryItem from './galleryItem';
 
 const Gallery = ({ images }) => {
 
+  if (!images.length) {
+    return (
+      <div>
+        <h3>No results found</h3>
+        <p>That search did not return any results, please try again.</p>
+      </div>
+    );
+  }
+
   return (
     <div className="photo-container">
       <h2>Results</h2>
