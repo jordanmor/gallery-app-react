7a445e79fafd93821618a2c6360fb1b360426a2d
Jordan Mor <jordanmor@gmail.com>
2018-09-17 11:32:35 -0400

    Improve message that displays when no search matches are found

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -5,7 +5,8 @@ const Gallery = ({ images }) => {
 
   if (!images.length) {
     return (
-      <div>
+      <div className="no-gifs">
+        <i className="material-icons icon-gif">sentiment_very_dissatisfied</i>
         <h3>No results found</h3>
         <p>That search did not return any results, please try again.</p>
       </div>
