d0f51b88201593465f7e344066f65a810c999887
Jordan Mor <jordanmor@gmail.com>
2018-09-17 12:11:19 -0400

    App displays loading indicator when new data fetched

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Gallery from './components/gallery';
 import Header from './components/header';
+import Loader from './components/loader';
 import apiKey from './.config';
 
 class App extends Component {
   state = {
-      images: []
+      images: [],
+      loading: true
   }
 
   componentDidMount() {
@@ -13,11 +15,13 @@ class App extends Component {
   }
 
   performSearch = (tag = 'orange leaves') => {
+    this.setState({ loading: true });
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tag}&per_page=24&format=json&nojsoncallback=1`)
       .then(res => res.json())
       .then(data => {
         this.setState({
-          images: data.photos.photo
+          images: data.photos.photo,
+          loading: false
         });
       });
   }
@@ -28,7 +32,11 @@ class App extends Component {
     return (
       <div className="container">
         <Header onSearch={this.performSearch} />
-        <Gallery images={images} />
+        {
+          this.state.loading
+          ? <Loader />
+          : <Gallery images={images} />
+        }
       </div>
     );
   }
