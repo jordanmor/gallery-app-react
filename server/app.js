70c75fade97950b8784c3119f4ffedacb3f28e01
Jordan Mor <jordanmor@gmail.com>
2018-12-04 13:09:59 -0500

    Add backend to project

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+// Serve static files from the React app
+const staticFiles = express.static(path.join(__dirname, '../../client/build'));
+// app.use(staticFiles);
+
+app.use('/*', staticFiles);
+
+// send 404 if no other route matched
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route Not Found'
+  });
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  // console.error(err.stack);
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: {}
+  });
+});
+
+// start listening on our port
+app.listen(port, () => {
+  console.log(`Express server is listening on port ${port}`);
+});
