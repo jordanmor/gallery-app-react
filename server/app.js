2cb07c6f12706ab38dda76956e37e56ad5327b5b
Jordan Mor <jordanmor@gmail.com>
2018-12-04 15:17:34 -0500

    Fix npm start path and server static files path

--- server/app.js
+++ server/app.js
@@ -7,8 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
 // Serve static files from the React app
-const staticFiles = express.static(path.join(__dirname, '../../client/build'));
-// app.use(staticFiles);
+const staticFiles = express.static(path.join(__dirname, './../client/build'));
 
 app.use('/*', staticFiles);
 
